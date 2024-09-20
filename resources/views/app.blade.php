2094996f34503978cf67eb18c46557181f4a7ab2
Megamia <luudat214@gmaill.com>
2024-09-20 10:22:43 +0700

    add tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>ThuocOnline</title>
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @vite('resources/js/app.jsx')
 </head>
 <body style="margin: 0">
